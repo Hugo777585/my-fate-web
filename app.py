10f96855738446a709762addd4e4d938fea0dcdc
Hugo777585 <dream53721@gmail.com>
2026-04-03 06:49:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -423,4 +423,3 @@ if module:
             col_dl1, col_dl2 = st.columns(2)
             col_dl1.download_button("📥 下載 PDF 版", data=pdf_bytes, file_name=f"{module}.pdf", mime="application/pdf")
             col_dl2.download_button("📥 下載純文字版", data=result.encode("utf-8"), file_name=f"{module}.txt")
-
